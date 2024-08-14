ffff42c92d26a006245b583c5e75050d1e7c8483
Christopher Parsons <786967+cwparsons@users.noreply.github.com>
2024-08-13 23:20:16 -0700

    Add MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import yaml from '@rollup/plugin-yaml';
 import pagefind from 'astro-pagefind';
-
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkGfm from 'remark-gfm';
@@ -10,37 +9,36 @@ import remarkToc from 'remark-toc';
 import remarkSmartypants from 'remark-smartypants';
 
 // @see https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://cwparsons.ca',
-
   markdown: {
-    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
-
-    remarkPlugins: [remarkGfm, [remarkToc, { maxDepth: 2 }], remarkSmartypants],
-
+    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, {
+      behavior: 'wrap'
+    }]],
+    remarkPlugins: [remarkGfm, [remarkToc, {
+      maxDepth: 2
+    }], remarkSmartypants],
     shikiConfig: {
       themes: {
         dark: 'github-dark',
-        light: 'github-light',
+        light: 'github-light'
       },
-      transformers: [
-        {
-          preprocess(code) {
-            if (code.endsWith('\n')) {
-              code = code.slice(0, -1);
-            }
-            return code;
-          },
-        },
-      ],
-    },
+      transformers: [{
+        preprocess(code) {
+          if (code.endsWith('\n')) {
+            code = code.slice(0, -1);
+          }
+          return code;
+        }
+      }]
+    }
   },
-
-  integrations: [pagefind(), sitemap()],
-
+  integrations: [pagefind(), sitemap(), mdx()],
   prefetch: true,
-
   vite: {
-    plugins: [yaml()],
-  },
+    plugins: [yaml()]
+  }
 });

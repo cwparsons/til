1e4789d623a71408bb17de07219e31f7dd0ef114
Christopher Parsons <cwlparsons@gmail.com>
2026-06-21 19:40:21 -0700

    Add author information and structured data to index page

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,9 @@
 export const SITE_DESCRIPTION = 'Stuff I need to write down.';
 export const SITE_TITLE = 'cwparsons.ca';
 export const SITE_WORDMARK = 'cwparsons.ca';
+export const SITE_AUTHOR = 'Christopher Parsons';
+export const SITE_SOCIAL_PROFILES = [
+  'https://www.linkedin.com/in/cwlparsons/',
+  'https://github.com/cwparsons',
+  'https://www.last.fm/user/Whtlw',
+];
